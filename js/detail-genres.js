b2a70a5df57d5564e4999a5e4dace5be19716b60
mariapaznobile <pnobile@udesa.edu.ar>
2023-11-13 23:55:23 -0300

    detail html terminado

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -7,6 +7,7 @@ window.addEventListener ('load', function (){
     //peliculas//
     let generoPeliculas = document.querySelector(".galeria") 
     let urlDetalleGen = `https://api.themoviedb.org/3/discover/movie?api_key=3c3df15ca351ec3735ad14140026cfb8&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${idGen}&with_watch_monetization_types=flatrate`;
+    console.log(urlDetalleGen);
     const options = {
         method: 'GET',
         headers: {
@@ -14,7 +15,7 @@ window.addEventListener ('load', function (){
           Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzNkZjE1Y2EzNTFlYzM3MzVhZDE0MTQwMDI2Y2ZiOCIsInN1YiI6IjY1NGE1MjQ3NTMyYWNiMDEzODBjOTczNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VmVu6YIU9OpS5RIJ_aIWxqW1O3P-a0xjYVJlzzRCA90'
         }
     };
-    console.log(`https://api.themoviedb.org/3/discover/movie?api_key=3c3df15ca351ec3735ad14140026cfb8&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${idGen}&with_watch_monetization_types=flatrate`);
+    //console.log(`https://api.themoviedb.org/3/discover/movie?api_key=3c3df15ca351ec3735ad14140026cfb8&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${idGen}&with_watch_monetization_types=flatrate`);
     fetch(urlDetalleGen,options)
     .then(function (response){
         return response.json();
@@ -26,7 +27,7 @@ window.addEventListener ('load', function (){
 
     for (let i = 0; i< arrayGeneros.length; i++){
         DetalleGen += `<div class="pelicula">
-        <a href="./detail-movie.html?id=${arrayGeneros[i].genre_ids}"><img src="https://image.tmdb.org/t/p/w500/${arrayGeneros[i].poster_path}" alt="pelis"></a>
+        <a href="./search-results.html?resultadosbusqueda=${arrayGeneros[i].title}"><img src="https://image.tmdb.org/t/p/w500/${arrayGeneros[i].poster_path}" alt="pelis"></a>
         <h4 class="titulos-peliculas">${arrayGeneros[i].title}</h4>
         <p class="fechas">${arrayGeneros[i].release_date}</p>
     </div>`
@@ -34,35 +35,36 @@ window.addEventListener ('load', function (){
     generoPeliculas.innerHTML = DetalleGen;
     })
     .catch(function (error){
-        console.log(error)
+        console.error(error)
     })
 
-    //series//
-   /* let url2 = 'https://api.themoviedb.org/3/discover/tv?api_key=3c3df15ca351ec3735ad14140026cfb8&language=en-US&with_genres=${idGen}'
-    let generoSeries = document.querySelector(".galeriatv")
+    // //series//
+    // let url2 = 'https://api.themoviedb.org/3/discover/tv?api_key=3c3df15ca351ec3735ad14140026cfb8&language=en-US&with_genres=${idGen}'
+    // let generoSeries = document.querySelector(".galeriatv")
     
-    fetch (url2)
-    .then  (function (response){
-        return response.json ();
-    })
-    .then (function (data){
-        let results = data.results
-        console.log(results);
-        console.log(data);
+    // fetch (url2)
+    // .then  (function (response){
+    //     return response.json ();
+    // })
+    // .then (function (data){
+    //     let results = data.results
+    //     console.log(results);
+    //     console.log(data);
 
-        let GeneroEspecifico = ''
+    //     let GeneroEspecifico = ''
     
-    for (let i = 0; i < results.length; i++){
-        GeneroEspecifico += `<div class="series">
-        <a href="./detail-serie.html?id=${results[i].id}"><img src="https://image.tmdb.org/t/p/w500/${results[i].poster_path}" alt="series"></a>
-        <h4 class="titulos-series">${results[i].original_name}</h4>
-        <p class="fechas">${results[i].first_air_date}</p>
-    </div>`
-    }
-    generoSeries.innerHTML = GeneroEspecifico;
-    })
-    .catch (function (error){
-        console.log (error)
-    })*/
+    // for (let i = 0; i < results.length; i++){
+    //     GeneroEspecifico += `<div class="series">
+    //     <a href="./detail-serie.html?id=${results[i].id}"><img src="https://image.tmdb.org/t/p/w500/${results[i].poster_path}" alt="series"></a>
+    //     <h4 class="titulos-series">${results[i].original_name}</h4>
+    //     <p class="fechas">${results[i].first_air_date}</p>
+    // </div>`
+    // }
+    // generoSeries.innerHTML = GeneroEspecifico;
+    // })
+    // .catch (function (error){
+    //     console.log (error)
+    // })
+   
 
 })
