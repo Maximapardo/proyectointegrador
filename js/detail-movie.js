50198cb0e4e13495c1d29d2c4b9155f483cd2401
mariapaznobile <pnobile@udesa.edu.ar>
2023-11-14 23:16:28 -0300

    .

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -1,65 +1,42 @@
 window.addEventListener('load', function () {
-    let detalle = location.search;
-    let detalleObjeto = new URLSearchParams(detalle);
-    let movie_id = detalleObjeto.get('id');
+    let detallePeli = location.search;
+    let detalleGeneroObjeto = new URLSearchParams(detallePeli);
+    let idPeli = detallePeliObjeto.get ("id")
 
-    let url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=3c3df15ca351ec3735ad14140026cfb8&language=en-US`;
-
-    fetch(url)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (peliculaDetalle) {
-            console.log(peliculaDetalle);
-
-            document.querySelector('.title').innerHTML = peliculaDetalle.title;
-            document.querySelector('.overview').innerHTML = peliculaDetalle.overview;
-            document.querySelector('.image').src = `https://image.tmdb.org/t/p/w500/${peliculaDetalle.poster_path}`;
-            document.querySelector('.releasedate').innerHTML = peliculaDetalle.release_date;
-            document.querySelector('.runtime').innerHTML = peliculaDetalle.runtime;
-            document.querySelector('.rating').innerHTML = peliculaDetalle.vote_average;
-
-            for (var i = 0; i < peliculaDetalle.genres.length; i++) {
-                let genero = document.querySelector(".genero");
-                genero.innerHTML += `<a href="detail-genres.html?id=${peliculaDetalle.genres[i].id}"> ${peliculaDetalle.genres[i].name}</a>`
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-});
-
-
-    //recomendaciones//
-
-    let url2 = (`https://api.themoviedb.org/3/movie/${movie_id}/recommendations?api_key=3c3df15ca351ec3735ad14140026cfb8&language=en-US`)
-
-    fetch (url2)
-    .then (function (response){
-        return response.json ();
+    //peliculas//
+    let detallePeliculas = document.querySelector(".galeria") 
+    let urlDetallePeli = `https://api.themoviedb.org/3/discover/movie?api_key=3c3df15ca351ec3735ad14140026cfb8&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${idPeli}&with_watch_monetization_types=flatrate`;
+    console.log(urlDetalleGen);
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzNkZjE1Y2EzNTFlYzM3MzVhZDE0MTQwMDI2Y2ZiOCIsInN1YiI6IjY1NGE1MjQ3NTMyYWNiMDEzODBjOTczNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VmVu6YIU9OpS5RIJ_aIWxqW1O3P-a0xjYVJlzzRCA90'
+        }
+    };
+    //console.log(`https://api.themoviedb.org/3/discover/movie?api_key=3c3df15ca351ec3735ad14140026cfb8&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${idGen}&with_watch_monetization_types=flatrate`);
+    fetch(urlDetallePeli,options)
+    .then(function (response){
+        return response.json();
     })
     .then (function (data){
-        console.log (data);
-        let resultados= data.results 
-        let botonrecomendaciones = document.querySelector('.recomendaciones')
-        let recomendacion = document.querySelector('.galeriamovie')
+        let arrayDetalle = data.results
+        console.log(arrayDetalle);
+        let DetallePeli = ''
 
-        botonrecomendaciones.addEventListener('click', function(event){
-            event.preventDefault();
-
-            document.querySelector('reco').innerText = "Recomendaciones";
-            
-
-            for (let i = 0; i < resultados.length; i++) {
-                recomendacion.innerHTML += `<div class="pelicula">
-            <a href="./detail-movie.html?id=${resultados[i].id}"><img src="https://image.tmdb.org/t/p/w500/${resultados[i].poster_path}" alt="pelis"></a>
-            <h4 class="titulos-peliculas">${resultados[i].title}</h4>
-            <p class="fechas">${resultados[i].release_date}</p>
-        </div>`
-            }
-        })
-
+    for (let i = 0; i< arrayDetalle.length; i++){
+        DetallePeli += `<div class="pelicula">
+        <a href="./detail-movie.html?resultadosbusqueda=${arrayDetalle[i].title}"><img src="https://image.tmdb.org/t/p/w500/${arrayGeneros[i].poster_path}" alt="pelis"></a>
+        <h4 class="titulos-peliculas">${arrayDetalle[i].title}</h4>
+        <p class="fechas">${arrayDetalle[i].release_date}</p>
+    </div>`
+    }
+    detallePeliculas.innerHTML = DetallePelis;
     })
+    .catch(function (error){
+        console.error(error)
+    })
+})
 
 
 
