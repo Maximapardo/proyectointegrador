597832b9f527194594f0fb1e76dac20badd10dde
Maximapardo <maximapardo@udesa.edu.ar>
2023-11-17 10:31:48 -0300

    editamos js

--- js/search-results.js
+++ js/search-results.js
@@ -1,32 +1,37 @@
-const search=()=>{
+const search = () => {
+  const apiKey = '3c3df15ca351ec3735ad14140026cfb8'; // Reemplaza 'tu_clave_de_api' con tu clave real de la API
+  const url = 'https://api.themoviedb.org/3/search/movie?query=' + new URLSearchParams(window.location.search).get("resultadosbusqueda") + '&include_adult=false&language=en-US&page=1';
 
-  
-  const url = 'https://api.themoviedb.org/3/search/movie?query='+new URLSearchParams(window.location.search).get("resultadosbusqueda")+'&include_adult=false&language=en-US&page=1';
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
   fetch(url, options)
     .then(res => res.json())
-    .then(json =>{
+    .then(json => {
       console.log(json);
-     
-        document.getElementById("titlemovie").innerHTML= json.results[0].title;
-        document.getElementById("titledate").innerHTML=json.results[0].release_date;
-        document.getElementById("sinopsis").innerHTML=json.results[0].overview;
-        document.getElementById("generos").innerHTML=''; //json.results[0].title;
-        document.getElementById("calificacion").innerHTML=json.results[0].vote_average;
-        document.getElementById("image_movie").src='https://image.tmdb.org/t/p/w500/'+json.results[0].backdrop_path;
-        document.getElementById("calificacion").style ="--value:"+json.results[0].vote_average/2;
-        
-        
-      
+
+      document.getElementById("titlemovie").innerHTML = json.results[0].title;
+      document.getElementById("titledate").innerHTML = json.results[0].release_date;
+      document.getElementById("sinopsis").innerHTML = json.results[0].overview;
+      document.getElementById("generos").innerHTML = ''; //json.results[0].title;
+      document.getElementById("calificacion").innerHTML = json.results[0].vote_average;
+      document.getElementById("image_movie").src = 'https://image.tmdb.org/t/p/w500/' + json.results[0].backdrop_path;
+      document.getElementById("calificacion").style = "--value:" + json.results[0].vote_average / 2;
 
     })
     .catch(err => console.error('error:' + err));
 
-let queryString = location.search
-console.log(queryString)
+  let queryString = location.search;
+  console.log(queryString);
 
-let queryStringObj = new URLSearchParams(queryString)
-console.log(queryStringObj.get("resultadosbusqueda"))
+  let queryStringObj = new URLSearchParams(queryString);
+  console.log(queryStringObj.get("resultadosbusqueda"));
+};
 
-
-
- };
+// Llamada a la función
+search();
+ 
